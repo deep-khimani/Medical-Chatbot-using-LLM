0dc0b096406f29bf15170f5756647e74f537b0e2
deepkhimani01 <deep.khimani.btech2022@sitpune.edu.in>
2025-10-01 16:39:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Deep Khimani",
+    author_email="deepkhimani@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
